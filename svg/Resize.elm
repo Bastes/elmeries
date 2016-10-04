6f7f71157729ceec26ca91095176ea74c8ad0054
Michel Belleville <michel.belleville@gmail.com>
2016-10-04 23:46:20 +0200

    Example of how to react to the windows size

---
+++ svg/Resize.elm
@@ -0,0 +1,63 @@
+module Main exposing (..)
+import Window
+import Task
+import Html exposing (Html)
+import Html.App as App
+import Svg exposing (svg, text, text')
+import Svg.Attributes exposing (viewBox, width, height, x, y, style)
+
+
+main =
+    App.program
+        { init = init
+        , view = view
+        , update = update
+        , subscriptions = subscriptions
+        }
+
+
+
+-- MODEL
+
+
+type alias Model = ( Int, Int )
+
+
+defaultDimensions = ( 100, 100 )
+
+
+init : ( Model, Cmd Msg )
+init = ( defaultDimensions, Task.perform (\_ -> Fail) (\dimentions -> WindowResize dimentions) Window.size )
+
+
+-- UPDATE
+
+
+type Msg = WindowResize { width : Int, height : Int }
+         | Fail
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model =
+    case msg of
+        WindowResize { width, height } ->
+            ( ( width, height ), Cmd.none )
+        Fail ->
+            ( model, Cmd.none )
+
+
+
+-- SUBSCRIPTIONS
+
+
+subscriptions : Model -> Sub Msg
+subscriptions model = Window.resizes WindowResize
+
+
+-- VIEW
+
+
+view : Model -> Html Msg
+view ( w, h ) =
+    svg [ viewBox <| "0 0 " ++ (toString w) ++ " " ++ (toString h), width "100%", height "100%", style "position: fixed; left: 0%; top: 0%;" ]
+        [ text' [ x "2", y "15" ] [ text ((toString w) ++ " " ++ (toString h)) ] ]
