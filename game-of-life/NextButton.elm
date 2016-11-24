1f3325384ffa71d7d06e786e7e92bdbaf96a29be
Michel Belleville <michel.belleville@gmail.com>
2016-11-24 11:46:37 +0100

    factoring buttons

--- game-of-life/NextButton.elm
+++ game-of-life/NextButton.elm
@@ -26,52 +26,29 @@ type alias X =
 nextButton : Width -> Active -> msg -> Html msg
 nextButton iWidth active action =
     let
-        sWidth =
-            toString iWidth
-
-        svgViewBox =
-            viewBox <| "0 0 10 10"
-
-        cursorStyle =
-            case active of
-                False ->
-                    ""
-
-                True ->
-                    "cursor: pointer; "
-
-        svgStyle =
-            style <| cursorStyle ++ "width: " ++ sWidth ++ "px; height: " ++ sWidth ++ "px;"
-
-        frame =
-            rect [ y "0", x "0", height sWidth, width sWidth, fill "#777777" ] []
-
-        fillColor =
-            case active of
-                False ->
-                    "#bbbbbb"
-
-                True ->
-                    "#ffffff"
-
-        playSymbol =
-            [ polygon [ points "2,3 2,7 6,5", fill fillColor ] []
-            , polygon [ points "4,3 4,7 8,5", fill fillColor ] []
-            ]
-
-        actions =
-            case active of
-                False ->
-                    []
-
-                True ->
-                    [ onClick action ]
+        symbol =
+            \fillColor ->
+                [ polygon [ points "2,3 2,7 6,5", fill fillColor ] []
+                , polygon [ points "4,3 4,7 8,5", fill fillColor ] []
+                ]
     in
-        svg [ svgViewBox, svgStyle ] [ g actions (frame :: playSymbol) ]
+        button iWidth active symbol action
 
 
 prevButton : Width -> Active -> msg -> Html msg
 prevButton iWidth active action =
+    let
+        symbol =
+            \fillColor ->
+                [ polygon [ points "6,3 6,7 2,5", fill fillColor ] []
+                , polygon [ points "8,3 8,7 4,5", fill fillColor ] []
+                ]
+    in
+        button iWidth active symbol action
+
+
+button : Width -> Active -> (String -> List (Html msg)) -> msg -> Html msg
+button iWidth active symbol action =
     let
         sWidth =
             toString iWidth
@@ -101,11 +78,6 @@ prevButton iWidth active action =
                 True ->
                     "#ffffff"
 
-        playSymbol =
-            [ polygon [ points "6,3 6,7 2,5", fill fillColor ] []
-            , polygon [ points "8,3 8,7 4,5", fill fillColor ] []
-            ]
-
         actions =
             case active of
                 False ->
@@ -114,4 +86,4 @@ prevButton iWidth active action =
                 True ->
                     [ onClick action ]
     in
-        svg [ svgViewBox, svgStyle ] [ g actions (frame :: playSymbol) ]
+        svg [ svgViewBox, svgStyle ] [ g actions (frame :: (symbol fillColor)) ]
